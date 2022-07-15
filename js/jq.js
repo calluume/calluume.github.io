06cc24b7fee6b1f40afac2457932b686b28cffb4
calluume <cdevans98@gmail.com>
2022-07-15 18:54:17 +0100

    Deleted some files

--- js/jq.js
+++ js/jq.js
@@ -2,7 +2,6 @@ window.onload = function() {
     const showing = "translateY(-50%) rotate(-180deg)";
     const hiding = "translateY(-50%) rotate(0deg)";
 
-
     $('#project1body').on('show.bs.collapse', function () {
         document.getElementById('project1btn').style.transform = showing;
     })
@@ -15,7 +14,6 @@ window.onload = function() {
     $('#project2body').on('hide.bs.collapse', function () {
         document.getElementById('project2btn').style.transform = hiding;
     })
-
     $('#project3body').on('show.bs.collapse', function () {
         document.getElementById('project3btn').style.transform = showing;
     })
@@ -28,15 +26,4 @@ window.onload = function() {
     $('#project4body').on('hide.bs.collapse', function () {
         document.getElementById('project4btn').style.transform = hiding;
     })
-    
-    /*
-    const events = ['show.bs.collapse', 'hide.bs.collapse'];
-    const angles = ['-90', '0']
-    for (i=1; i<5; i++) {
-        for (j=0; j<2; j++) {
-            $('#project'+i+'body').on(events[j], function () {
-                document.getElementById('project'+i+'btn').style.transform = "translateY(-50%) rotate("+angles[j]+"deg)";
-            }) 
-        }
-    }*/
 }
