919d34c6a6b29f2457be49381dc2eba6e68db2cf
calluume <cdevans98@gmail.com>
2022-09-19 23:23:01 +0100

    Changed multi-language support
    Text from any language can now added to the site from the loadText.js file to avoid having multiple index/project page files, and preferred language is now stored locally between visits

--- js/js.js
+++ js/js.js
@@ -3,35 +3,6 @@ function zeroPad(num) {
     return Array(+(zero > 0 && zero)).join("0") + num;
 }
 
-window.onload = function() {
-    frMonths = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
-    enMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-    if (document.getElementById('last-updated')) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let repos = JSON.parse(this.responseText);
-            repos.forEach((repo)=>{
-                if (repo.name == "stebey.github.io") {
-                    date = new Date(repo.pushed_at)
-                    if (window.location.pathname.includes('fr')) {
-                        month = frMonths[date.getMonth()]
-                        at = 'à'
-                    } else {
-                        month = enMonths[date.getMonth()]
-                        at = 'at'
-                    }
-                    document.getElementById('last-updated').innerHTML += ` <i>${date.getDate()} ${month}, ${date.getFullYear()} ${at} ${zeroPad(date.getHours())}:${zeroPad(date.getMinutes())}</i>`
-                }
-            });
-        }
-        };
-        xhttp.open("GET", "https://api.github.com/users/calluume/repos", true);
-        xhttp.send();
-    }
-};
-
 function getOffset(el) {
     const rect = el.getBoundingClientRect();
     return rect.top + window.scrollY
